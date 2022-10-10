51aa23ec6f1fda0fdbce9370aaa8bce43cf87976
smska2022 <2009sms@rambler.ru>
2022-10-10 10:53:48 +0300

    Работа №2 Продвинутый сервер

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const server = express()
+const path = require('path');
+const PORT = 3000
+
+server.get('/', function (req, res) {
+    res.sendFile(path.join(process.cwd(), 'index.html'))
+})
+
+    // this.app.use(express.static(path.join(__dirname, "index.html")));
+server.listen(PORT)
+console.log('http://127.0.0.1:3000')
+console.log('http://localhost:3000')
