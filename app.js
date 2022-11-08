dc6ba7bdbebb1f674a567d6a79e23d8ea48b6010
SmsKa2022 <2009sms@rambler.ru>
2022-11-08 23:04:09 +0300

    Черновик

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const server = express()
 const path = require('path');
 const PORT = 3000
+server.use(express.static(__dirname + "/static"));
+
 
 server.get('/', function (req, res) {
     res.sendFile(path.join(process.cwd(), 'index.html'))
 })
+
 
     // this.app.use(express.static(path.join(__dirname, "index.html")));
 server.listen(PORT)
